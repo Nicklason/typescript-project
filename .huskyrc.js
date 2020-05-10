c52a61c7513f1336c21a7cf2df610b5f78f6d2c3
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2020-05-10 02:05:42 +0200

    ci: set up commitlint
    Updated hooks and made eslint ignore .js files

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "hooks": {
-        "pre-commit": "lint-staged",
-        "pre-push": "npm run lint"
+        "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
+        "pre-commit": "lint-staged"
     }
 };
